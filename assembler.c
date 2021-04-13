cbbc9995d4648766bae6b7690e13a3254e71a869
csergen <info.sergencepoglu@gmail.com>
2021-04-13 22:11:42 +0300

    041321

--- assembler.c
+++ assembler.c
@@ -1,28 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <errno.h>
-
-#include <util.h>
-#include <assembler.h>
-
+#include "util.h"
+#include "assembler.h"
+#include "args.h"
 
 int main(int argc, char **argv) 
 {
-    char m_file_path[FILENAME_MAX] = "";
-
-    if (argc > 2)
-    {
-        perror("Too many argument");
-        exit(EXIT_FAILURE);
-    } else if (argc == 2)
-    {
-        strcpy(m_file_path, argv[1]);
-    } else {
-        printf("Enter file path: ");
-        fscanf(stdin, "%s", m_file_path);
-    }
+    char* m_file_path = argparse(argc, argv);
 
     StreamObject *streamObject = open_stream(m_file_path, "r");
     char *f_source = read_stream(streamObject);
