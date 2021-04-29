db3bba08adc82f36439e802ced156bd877bd069f
csergen <info.sergencepoglu@gmail.com>
2021-04-29 16:12:06 +0300

    NEW

--- init/vm.c
+++ init/vm.c
@@ -16,613 +16,259 @@
 #define INSTRUCTION_SIZE 0x8
 
 // MEMORY OFFSETS
-static unsigned int CODE_SEGMENT_OFFSET = 220;
-static unsigned int DATA_SEGMENT_BEGIN = 221;
+static unsigned int CODE_SEGMENT_BEGIN = 0x01;
+static unsigned int CODE_SEGMENT_END;
+
+static unsigned int DATA_SEGMENT_BEGIN;
+static unsigned int DATA_SEGMENT_END = 0xff;
 
 // REGISTERS
-static short int RAX[INSTRUCTION_SIZE];
-static short int RBX[INSTRUCTION_SIZE];
-static short int RCX[INSTRUCTION_SIZE];
-static short int RDX[INSTRUCTION_SIZE];
-static short int RIR[INSTRUCTION_SIZE];
-static short int RAR[INSTRUCTION_SIZE];
-static short int RDR[INSTRUCTION_SIZE];
-static short int RTR[INSTRUCTION_SIZE];
-static short int RPC[INSTRUCTION_SIZE];
+static short int RAX = 0x00;
+static short int RBX = 0x00;
+static short int RCX = 0x00;
+static short int RDX = 0x00;
+static short int RIR = 0x00;
+static short int RAR = 0x00;
+static short int RDR = 0x00;
+static short int RTR = 0x00;
+static short int RPC = 0x01;
 
 // MEMORY
-static short int MEMORY[MEMORY_SIZE][INSTRUCTION_SIZE];
+static char MEMORY[MEMORY_SIZE][INSTRUCTION_SIZE+1];
 
-// ############################# UTILS #################################
-static void
-BIN(short int hex, short int buffer[8])
+
+
+
+// ##################### BIN to HEX | HEX TO BIN #####################
+static char *BIN(const short int hex)
 {
-    char chr_hex[2];
-    char ch1;
-    char ch2;
+    char *binary = (char*)malloc(9);
+    char *hex_buffer = malloc(3);
 
-    sprintf(chr_hex, "%0x", hex);
+    sprintf(hex_buffer, "%0X", hex);
 
-    if (strlen(chr_hex) == 1)
+    int hex_len = strlen(hex_buffer);
+
+    if (hex_len == 1)
     {
-        ch1 = '0';
-        ch2 = chr_hex[0];
+        char temp = hex_buffer[0];
+        hex_buffer[1] = temp;
+        hex_buffer[0] = '0';
+        hex_len = strlen(hex_buffer);
     }
-    else
-    {
-        ch1 = chr_hex[0];
-        ch2 = chr_hex[1];
-    }
+
+    char ch1 = hex_buffer[1];
+    char ch2 = hex_buffer[0];
+
+    char *bin1 = malloc(sizeof(char)*4);
+    char *bin2 = malloc(sizeof(char)*4);
 
     switch (ch1)
     {
     case '0':
-        buffer[0] = 0;
-        buffer[1] = 0;
-        buffer[2] = 0;
-        buffer[3] = 0;
+        bin1 = "0000";
         break;
     case '1':
-        buffer[0] = 0;
-        buffer[1] = 0;
-        buffer[2] = 0;
-        buffer[3] = 1;
+        bin1 = "0001";
         break;
     case '2':
-        buffer[0] = 0;
-        buffer[1] = 0;
-        buffer[2] = 1;
-        buffer[3] = 0;
+        bin1 = "0010";
         break;
     case '3':
-        buffer[0] = 0;
-        buffer[1] = 0;
-        buffer[2] = 1;
-        buffer[3] = 1;
+        bin1 = "0011";
         break;
     case '4':
-        buffer[0] = 0;
-        buffer[1] = 1;
-        buffer[2] = 0;
-        buffer[3] = 0;
+        bin1 = "0100";
         break;
     case '5':
-        buffer[0] = 0;
-        buffer[1] = 1;
-        buffer[2] = 0;
-        buffer[3] = 1;
+        bin1 = "0101";
         break;
     case '6':
-        buffer[0] = 0;
-        buffer[1] = 1;
-        buffer[2] = 1;
-        buffer[3] = 0;
+        bin1 = "0110";
         break;
     case '7':
-        buffer[0] = 0;
-        buffer[1] = 1;
-        buffer[2] = 1;
-        buffer[3] = 1;
+        bin1 = "0111";
         break;
     case '8':
-        buffer[0] = 1;
-        buffer[1] = 0;
-        buffer[2] = 0;
-        buffer[3] = 0;
+        bin1 = "1000";
         break;
     case '9':
-        buffer[0] = 1;
-        buffer[1] = 0;
-        buffer[2] = 0;
-        buffer[3] = 1;
+        bin1 = "1001";
         break;
-    case 'A':
     case 'a':
-        buffer[0] = 1;
-        buffer[1] = 0;
-        buffer[2] = 1;
-        buffer[3] = 0;
+    case 'A':
+        bin1 = "1010";
         break;
-    case 'B':
     case 'b':
-        buffer[0] = 1;
-        buffer[1] = 0;
-        buffer[2] = 1;
-        buffer[3] = 1;
+    case 'B':
+        bin1 = "1011";
         break;
-    case 'C':
     case 'c':
-        buffer[0] = 1;
-        buffer[1] = 1;
-        buffer[2] = 0;
-        buffer[3] = 0;
+    case 'C':
+        bin1 = "1100";
         break;
-    case 'D':
     case 'd':
-        buffer[0] = 1;
-        buffer[1] = 1;
-        buffer[2] = 0;
-        buffer[3] = 1;
+    case 'D':
+        bin1 = "1101";
         break;
-    case 'E':
     case 'e':
-        buffer[0] = 1;
-        buffer[1] = 1;
-        buffer[2] = 1;
-        buffer[3] = 0;
+    case 'E':
+        bin1 = "1110";
         break;
-    case 'F':
     case 'f':
-        buffer[0] = 1;
-        buffer[1] = 1;
-        buffer[2] = 1;
-        buffer[3] = 1;
+    case 'F':
+        bin1 = "1111";
         break;
     }
 
     switch (ch2)
     {
     case '0':
-        buffer[4] = 0;
-        buffer[5] = 0;
-        buffer[6] = 0;
-        buffer[7] = 0;
+        bin2 = "0000";
         break;
     case '1':
-        buffer[4] = 0;
-        buffer[5] = 0;
-        buffer[6] = 0;
-        buffer[7] = 1;
+        bin2 = "0001";
         break;
     case '2':
-        buffer[4] = 0;
-        buffer[5] = 0;
-        buffer[6] = 1;
-        buffer[7] = 0;
+        bin2 = "0010";
         break;
     case '3':
-        buffer[4] = 0;
-        buffer[5] = 0;
-        buffer[6] = 1;
-        buffer[7] = 1;
+        bin2 = "0011";
         break;
     case '4':
-        buffer[4] = 0;
-        buffer[5] = 1;
-        buffer[6] = 0;
-        buffer[7] = 0;
+        bin2 = "0100";
         break;
     case '5':
-        buffer[4] = 0;
-        buffer[5] = 1;
-        buffer[6] = 0;
-        buffer[7] = 1;
+        bin2 = "0101";
         break;
     case '6':
-        buffer[4] = 0;
-        buffer[5] = 1;
-        buffer[6] = 1;
-        buffer[7] = 0;
+        bin2 = "0110";
         break;
     case '7':
-        buffer[4] = 0;
-        buffer[5] = 1;
-        buffer[6] = 1;
-        buffer[7] = 1;
+        bin2 = "0111";
         break;
     case '8':
-        buffer[4] = 1;
-        buffer[5] = 0;
-        buffer[6] = 0;
-        buffer[7] = 0;
+        bin2 = "1000";
         break;
     case '9':
-        buffer[4] = 1;
-        buffer[5] = 0;
-        buffer[6] = 0;
-        buffer[7] = 1;
+        bin2 = "1001";
         break;
-    case 'A':
     case 'a':
-        buffer[4] = 1;
-        buffer[5] = 0;
-        buffer[6] = 1;
-        buffer[7] = 0;
+    case 'A':
+        bin2 = "1010";
         break;
-    case 'B':
     case 'b':
-        buffer[4] = 1;
-        buffer[5] = 0;
-        buffer[6] = 1;
-        buffer[7] = 1;
+    case 'B':
+        bin2 = "1011";
         break;
-    case 'C':
     case 'c':
-        buffer[4] = 1;
-        buffer[5] = 1;
-        buffer[6] = 0;
-        buffer[7] = 0;
+    case 'C':
+        bin2 = "1100";
         break;
-    case 'D':
     case 'd':
-        buffer[4] = 1;
-        buffer[5] = 1;
-        buffer[6] = 0;
-        buffer[7] = 1;
+    case 'D':
+        bin2 = "1101";
         break;
-    case 'E':
     case 'e':
-        buffer[4] = 1;
-        buffer[5] = 1;
-        buffer[6] = 1;
-        buffer[7] = 0;
+    case 'E':
+        bin2 = "1110";
         break;
-    case 'F':
     case 'f':
-        buffer[4] = 1;
-        buffer[5] = 1;
-        buffer[6] = 1;
-        buffer[7] = 1;
+    case 'F':
+        bin2 = "1111";
         break;
     }
+
+    strcpy(binary, bin2);
+    strcat(binary, bin1);
+    free(hex_buffer);
+    binary[8] = '\0';
+
+    return binary;
 }
 
-static short int
-HEX(short int buffer[8])
+static short int HEX(const char *binary)
 {
-    char other[8];
-
-    for (int i = 0; i < 8; i++)
-        other[i] = buffer[i] + '0';
-
-    return strtol(other, NULL, 2);
+    return strtol(binary, NULL, 2);
 }
 
-static void
-set(short int _dest[8],
-    short int _src)
+// ######################### MEMORY DUMP ############################
+static void MEMDUMP()
 {
-    short int _bin_src[8];
-    BIN(_src, _bin_src);
+    printf(YELLOW"\n──────────Memory────────────────────────────────────────\n"RESET);
 
-    for (int i = 0; i < 8; i++)
-        _dest[i] = _bin_src[i];
-}
+    int i, j = 1;
+    printf("0\t%s\n", MEMORY[0]);
 
-static void
-reset(short int _other[8])
-{
-    for (int i = 0; i < 8; i++)
-        _other[i] = 0;
-}
-
-static void
-increase(short int _other[8])
-{
-    short int temp = HEX(_other);
-    temp++;
-
-    BIN(temp, _other);
-}
-
-static void and (short int _src1[8], short int _src2[8])
-{
-    for (int i = 7; i >= 0; i--)
-        _src1[i] = _src1[i] & _src2[i];
-}
-
-static void or (short int _src1[8], short int _src2[8])
-{
-    for (int i = 7; i >= 0; i--)
-        _src1[i] = _src1[i] | _src2[i];
-}
-
-static void not(short int _src[8])
-{
-    for (int i = 7; i >= 0; i--)
-        _src[i] = _src[i] ? 0 : 1;
-}
-
-
-// ############################ DUMP ####################################
-static void
-MEMDUMP()
-{
-    printf(YELLOW"\n──────Memory───────────────────────────────────────────\n"RESET);
-    int i, j;
-
-    printf("%0x\t", 0);
-    for (i = 0; i < 8; i++)
-        printf("%d", MEMORY[0][i]);
-    printf("\n");
-
-    for (i = 1; i < CODE_SEGMENT_OFFSET+15; i += 2)
+    for (i = 1; i <= CODE_SEGMENT_END+5; i+=2)
     {
-        printf("%0x\t", i);
-        if (HEX(RPC) == i) {
-        
-            for (j = 0; j < 8; j++)
-                printf(GRN"%d", MEMORY[i][j]);
-            printf(" ");
-            for (j = 0; j < 8; j++)
-                printf("%d", MEMORY[i + 1][j]);
-            printf(RESET);
-        } else if (i > CODE_SEGMENT_OFFSET) {
-            for (j = 0; j < 8; j++) 
-                printf("?");
-            printf(" ");
-            for (j = 0; j < 8; j++) 
-                printf("?");
-        }
+        printf("%0X\t", i);
+        if (RPC == i)
+            printf(GRN"%s %s\n"RESET, MEMORY[j], MEMORY[j+1]);
         else
-        {
-            for (j = 0; j < 8; j++)
-                printf("%d", MEMORY[i][j]);
-            printf(" ");
-            for (j = 0; j < 8; j++)
-                printf("%d", MEMORY[i + 1][j]);
-        }
-        printf("\n");
+            printf("%s %s\n", MEMORY[j], MEMORY[j+1]);
+        j+=2;
     }
+
 }
 
-static void 
-REGDUMP() {
+// ########################### REGISTER DUMP ############################
+static void REGDUMP() {
     printf(BLUE"\n──────Registers───────────────────────────────────────────\n"RESET);
-    printf("RAX: 0x%0x(%d)\t", HEX(RAX), HEX(RAX)); 
-    printf("RBX: 0x%0x(%d)\t", HEX(RBX), HEX(RBX)); 
-    printf("RCX: 0x%0x(%d)\t", HEX(RCX), HEX(RCX)); 
-    printf("RDX: 0x%0x(%d)\n", HEX(RDX), HEX(RDX)); 
-    printf("RIR: 0x%0x(%d)\t", HEX(RIR), HEX(RIR)); 
-    printf("RAR: 0x%0x(%d)\t", HEX(RAR), HEX(RAR)); 
-    printf("RDR: 0x%0x(%d)\t", HEX(RDR), HEX(RDR)); 
-    printf("RTR: 0x%0x(%d)\n", HEX(RTR), HEX(RTR)); 
-    printf("RPC: 0x%0x(%d)\n", HEX(RPC), HEX(RPC));
+    printf("AX\t%s\t%0x\t%d\n", BIN(RAX), RAX, RAX); 
+    printf("BX\t%s\t%0x\t%d\n", BIN(RBX), RBX, RBX); 
+    printf("CX\t%s\t%0x\t%d\n", BIN(RCX), RCX, RCX); 
+    printf("DX\t%s\t%0x\t%d\n\n", BIN(RDX), RDX, RDX); 
+    printf("IR\t%s\t%0x\t%d\n", BIN(RIR), RIR, RIR); 
+    printf("AR\t%s\t%0x\t%d\n", BIN(RAR), RAR, RAR); 
+    printf("DR\t%s\t%0x\t%d\n", BIN(RDR), RDR, RDR); 
+    printf("TR\t%s\t%0x\t%d\n\n", BIN(RTR), RTR, RTR); 
+    printf("PC\t%s\t%0x\t%d\n", BIN(RPC), RPC, RPC);
 }
 
-// ###################### FETCH-DECODE-EXECUTE CYCLE ####################
-static void
-fetch_decode_execute()
+static void set()
 {
-    /* ======================== FETCH ========================== */
-    // for OPCODE, REGISTER and MODE
-    set(RAR, HEX(RPC));              // RAR <-- RPC
-    set(RIR, HEX(MEMORY[HEX(RAR)])); // RIR <-- M[RAR]
-    increase(RPC);                   // RPC <-- RPC + 1
 
-    // for ADDRESS
-    set(RAR, HEX(RPC));              // RAR <-- RPC
-    set(RAR, HEX(MEMORY[HEX(RAR)])); // RAR <-- M[RAR]
-    increase(RPC);                   // RPC <-- RPC
-
-    /* ======================== DECODE ========================== */
-    //OPCODE(4)
-    unsigned char *opcode_char = (unsigned char *)malloc(5);
-    unsigned char *register_char = (unsigned char *)malloc(3);
-    unsigned char *mode_char = (unsigned char *)malloc(3);
-
-    short int opcode_hex;
-    short int register_hex;
-    short int mode_hex;
-
-    // OPCODE(4)
-    opcode_char[0] = RIR[0] + '0';
-    opcode_char[1] = RIR[1] + '0';
-    opcode_char[2] = RIR[2] + '0';
-    opcode_char[3] = RIR[3] + '0';
-    opcode_char[4] = '\0';
-    opcode_hex = strtol(opcode_char, NULL, 2);
-    free(opcode_char);
-
-    // DESTINATION REGISTER(2)
-    register_char[0] = RIR[4] + '0';
-    register_char[1] = RIR[5] + '0';
-    register_char[2] = '\0';
-    register_hex = strtol(register_char, NULL, 2);
-    free(register_char);
-
-    // MODE(2)
-    mode_char[0] = RIR[6] + '0';
-    mode_char[1] = RIR[7] + '0';
-    mode_char[3] = '\0';
-    mode_hex = strtol(mode_char, NULL, 2);
-    free(mode_char);
-
-    /* ======================== EXECUTE ========================== */
-    short int src_hex;
-    switch (mode_hex)
-    {
-    // register
-    case MR:
-        printf("\nselected mode: MR\n");
-        src_hex = HEX(RAR);
-        switch (src_hex)
-        {
-        case AX:
-            printf("selected AX, RDR <-- RAX: %0x\n", HEX(RAX));
-            set(RDR, HEX(RAX));
-            break;
-        case BX:
-            printf("selected BX, RDR <-- RBX: %0x\n", HEX(RBX));
-            set(RDR, HEX(RBX));
-            break;
-        case CX:
-            printf("selected CX, RDR <-- RCX: %0x\n", HEX(RCX));
-            set(RDR, HEX(RCX));
-            break;
-        case DX:
-            printf("selected DX, RDR <-- RDX: %0x\n", HEX(RDX));
-            set(RDR, HEX(RDX));
-            break;
-        }
-        break;
-    case MI:
-        printf("\nselected mode: MI\n");
-        set(RDR, HEX(RAR));
-        break;
-    case MM:
-        break;
-    case MB:
-        set(RDR, HEX(RAR));
-        break;
-    }
-
-    switch (opcode_hex)
-    {
-    case HRK:
-        switch (register_hex)
-        {
-        case AX:
-            set(RAX, HEX(RDR));
-            break;
-        case BX:
-            set(RBX, HEX(RDR));
-            break;
-        case CX:
-            set(RCX, HEX(RDR));
-            break;
-        case DX:
-            set(RDX, HEX(RDR));
-            break;
-        }
-        break;
-    case TOP:
-        switch (register_hex)
-        {
-        case AX:
-            // RAX + RDR
-            set(RAX, HEX(RAX) + HEX(RDR));
-            set(MEMORY[0], HEX(RAX));
-            break;
-        case BX:
-            // RBX + RDR
-            set(RBX, HEX(RBX) + HEX(RDR));
-            set(MEMORY[0], HEX(RBX));
-            break;
-        case CX:
-            // RCX + RDR;
-            set(RCX, HEX(RCX) + HEX(RDR));
-            set(MEMORY[0], HEX(RCX));
-            break;
-        case DX:
-            // RDX + RDR
-            set(RDX, HEX(RDX) + HEX(RDR));
-            set(MEMORY[0], HEX(RDX));
-            break;
-        }
-        break;
-    case CRP:
-        switch (register_hex)
-        {
-        case AX:
-            // RAX * RDR
-            set(RAX, HEX(RAX) * HEX(RDR));
-            set(MEMORY[0], HEX(RAX));
-            break;
-        case BX:
-            // RBX * RDR
-            set(RBX, HEX(RBX) * HEX(RDR));
-            set(MEMORY[0], HEX(RBX));
-            break;
-        case CX:
-            // RCX * RDR;
-            set(RCX, HEX(RCX) * HEX(RDR));
-            set(MEMORY[0], HEX(RCX));
-            break;
-        case DX:
-            // RDX * RDR
-            set(RDX, HEX(RDX) * HEX(RDR));
-            set(MEMORY[0], HEX(RDX));
-            break;
-        }
-        break;
-    case CIK:
-        switch (register_hex)
-        {
-        case AX:
-            // RAX - RDR
-            set(RAX, HEX(RAX) - HEX(RDR));
-            set(MEMORY[0], HEX(RAX));
-            break;
-        case BX:
-            // RBX - RDR
-            set(RBX, HEX(RBX) - HEX(RDR));
-            set(MEMORY[0], HEX(RBX));
-            break;
-        case CX:
-            // RCX - RDR:
-            set(RCX, HEX(RCX) - HEX(RDR));
-            set(MEMORY[0], HEX(RCX));
-            break;
-        case DX:
-            // RDX - RDR
-            set(RDX, HEX(RDX) - HEX(RDR));
-            set(MEMORY[0], HEX(RDX));
-            break;
-        }
-        break;
-    case BOL:
-        switch (register_hex)
-        {
-        case AX:
-            // RAX / RDR
-            set(RAX, HEX(RAX) / HEX(RDR));
-            set(MEMORY[0], HEX(RAX));
-            break;
-        case BX:
-            // RBX / RDR
-            set(RBX, HEX(RBX) / HEX(RDR));
-            set(MEMORY[0], HEX(RBX));
-            break;
-        case CX:
-            // RCX / RDR:
-            set(RCX, HEX(RCX) / HEX(RDR));
-            set(MEMORY[0], HEX(RCX));
-            break;
-        case DX:
-            // RDX / RDR
-            set(RDX, HEX(RDX) / HEX(RDR));
-            set(MEMORY[0], HEX(RDX));
-            break;
-        }
-        break;
-    case SP:
-        if (HEX(MEMORY[0]) > 0)
-            set(RPC, HEX(RDR));
-    }
 }
 
-// ############################# MANAGER ################################
-static void
-init()
+// ####################### FETCH & DECODE & EXECUTE ######################
+static void ftdcex()
 {
-    reset(RAX);
-    reset(RBX);
-    reset(RCX);
-    reset(RDX);
-    reset(RIR);
-    reset(RDR);
-    reset(RAR);
-    reset(RTR);
+    RAR = RPC;
+    RIR = HEX(MEMORY[RAR]);
+    RPC++;
 
-    CODE_SEGMENT_OFFSET = HEX(RPC);
-    DATA_SEGMENT_BEGIN = CODE_SEGMENT_OFFSET + 1;
-
-    set(RPC, 0x1);
+    RAR = RPC;
+    RAR = HEX(MEMORY[RAR]);
+    RPC++;
 }
 
-static void
-run()
+static void init()
 {
-    while (HEX(RPC) <= CODE_SEGMENT_OFFSET)
+    RAX = 0x00;
+    RBX = 0x00;
+    RCX = 0x00;
+    RDX = 0x00;
+    RIR = 0x00;
+    RAR = 0x00;
+    RDR = 0x00;
+    RTR = 0x00;
+
+    CODE_SEGMENT_END = RPC-1;
+    DATA_SEGMENT_BEGIN = RPC;
+
+    RPC = 0x01;
+}
+
+static void run()
+{
+    while (RPC < CODE_SEGMENT_END)
     {
         printf("\e[1;1H\e[2J");
         MEMDUMP();
-
+        ftdcex();
         REGDUMP();
         getc(stdin);
-
-        fetch_decode_execute();
     }
 }
 
@@ -631,20 +277,20 @@ void load_program(char *src_file)
     StreamObject *streamObject = open_stream(src_file, "r");
     char buffer[3];
     short int hex;
-
-    reset(RPC);
-    increase(RPC);
+    char *temp;
 
     while (fgets(buffer, sizeof(buffer), streamObject->stream))
     {
         if (*buffer != '\n')
         {
             hex = strtol(buffer, NULL, 16);
-            BIN(hex, MEMORY[HEX(RPC)]);
-            increase(RPC);
+            temp = BIN(hex);
+            strcpy(MEMORY[RPC++], temp);
+            free(temp);
         }
     }
 
     init();
     run();
+
 }
